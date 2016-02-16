1f6f8adf19661c2a9ca42574dd41dbacc375ad7b
Mukesh Sharma <er.mukeshsharma.rj21@gmail.com>
2016-02-16 12:33:52 +0530

    app code commited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChattyApp::Application.config.secret_key_base = '66eda67fb51fe38f1f278e25ea5250e29d1a4c8d810ad9916c6cdc96f51f4264534c7cdb4e32e87b6de6ea1ff4dbdc9937552be635042a55d3a591b11fe774c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChattyApp::Application.config.session_store :cookie_store, key: '_chatty_app_session'
